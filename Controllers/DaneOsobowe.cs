b8bf67f5362b37c5d94a04f6252df60f50ed3b9d
michalgibajlo <michal.gibajlo@gmail.com>
2022-07-01 15:44:20 +0200

    first comment

---
+++ Controllers/DaneOsobowe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebServices.Controllers
+{
+    public class DaneOsobowe
+    {
+            public string imie { get; set; }
+            public string nazwisko { get; set; }
+            public string adres { get; set; }
+    }
+}
